d14d4bd31d32cc12261b3a84180ba0babb4eb7fc
vitormvrdev <vitormvr2001@gmail.com>
2025-03-23 09:35:01 +0000

    Add constructors and methods to Seller and SellerCollection
    Updated the `Seller` class to include a default and a parameterized constructor, along with properties for `SellerId` and `Name`. Introduced a `GetSeller` method for retrieving seller details by ID.
    
    Enhanced the `SellerCollection` class with a new constructor that initializes the collection from a `DataTable`, populating it with `Seller` objects based on the data rows.

--- BusinessLayer/Seller.cs
+++ BusinessLayer/Seller.cs
@@ -6,15 +6,71 @@ namespace BusinessLayer
     {
         #region Constructors
 
+        // Default Constructor
 
+        public Seller()
+        {
+            this.sellerId = 0;
+            this.name = string.Empty;
+        }
+
+        // Constructor with parameters
+
+        public Seller(int sellerId, string name)
+        {
+            this.sellerId = sellerId;
+            this.name = name;
+        }
 
         #endregion
 
         #region Properties
+        /*
+        [SellerID] [int] NOT NULL,
+	    [Name] [nvarchar](100) NOT NULL,
+        */
+
+        private int sellerId;
+
+        public int SellerId
+        {
+            get { return sellerId; }
+            set { sellerId = value; }
+        }
+
+        private string name;
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+
 
         #endregion
 
         #region Methods
+
+        // Method to get sellers by ID
+
+        public Seller GetSeller(int sellerId)
+        {
+            Seller seller = null;
+
+            string name = string.Empty;
+            string error = string.Empty;
+
+
+            bool result = DataLayer.Seller.GetSeller(sellerId, ref name, out error);
+
+            if (result)
+            {
+                seller = new Seller(sellerId, name);
+            }
+
+            return seller;
+        }
 
         #endregion
     }
--- BusinessLayer/SellerCollection.cs
+++ BusinessLayer/SellerCollection.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Data;
 using System.Text;
 
 namespace BusinessLayer
@@ -9,6 +10,23 @@ namespace BusinessLayer
     {
         public SellerCollection()
         {
+        }
+
+        public SellerCollection(DataTable dataTable)
+            : this()
+        {
+            if (dataTable != null)
+            {
+                foreach (DataRow dataRow in dataTable.Rows)
+                {
+                    Seller seller = new Seller();
+
+                    seller.SellerId = dataRow.Field<int>("SellerID");
+                    seller.Name = dataRow.Field<string>("Name");
+
+                    this.Add(seller);
+                }
+            }
         }
     }
 }
